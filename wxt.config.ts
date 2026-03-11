3d9d6f518bff2365590c6e19a40eba5f4eb1989d
Francis Breidenbach <8886566+cheefbird@users.noreply.github.com>
2026-03-10 19:50:10 -0700

    fix(firefox): add required browser settings

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,14 +1,22 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte'],
+  srcDir: "src",
+  modules: ["@wxt-dev/module-svelte"],
   zip: {
-    name: 'grody-github',
-    artifactTemplate: '{{name}}-v{{version}}-{{browser}}.zip',
+    name: "grody-github",
+    artifactTemplate: "{{name}}-v{{version}}-{{browser}}.zip",
   },
   manifest: {
-    permissions: ['storage'],
+    permissions: ["storage"],
+    browser_specific_settings: {
+      gecko: {
+        id: "grody-github@cheefbird",
+        data_collection_permissions: {
+          required: ["browsingActivity", "authenticationInfo"],
+        },
+      },
+    },
   },
 });
